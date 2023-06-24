dc783d0e366cd2b2b46123e9965e0ae53c5c842c
master <kristiyanvasilev96@gmail.com>
2023-06-24 11:27:00 +0300

    add comment if not empty str

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -1,50 +1,70 @@
-﻿const offerId = document.getElementById('offerId').value
-const commentForm = document.getElementById('commentForm')
-commentForm.addEventListener("submit", handleFormSubmission)
+﻿const offerId = document.getElementById('offerId').value;
+const commentForm = document.getElementById('commentForm');
+const postCommentButton = document.getElementById('postComment');
+const messageInput = document.getElementById('message');
+commentForm.addEventListener("submit", handleFormSubmission);
 
-const csrfHeaderName = document.head.querySelector('[name=_csrf_header]').content
-const csrfHeaderValue = document.head.querySelector('[name=_csrf]').content
+const csrfHeaderName = document.head.querySelector('[name=_csrf_header]').content;
+const csrfHeaderValue = document.head.querySelector('[name=_csrf]').content;
 
-const commentContaier = document.getElementById('commentCtnr')
+const commentContaier = document.getElementById('commentCtnr');
 
 async function handleFormSubmission(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const messageVal = document.getElementById('message').value
+    const messageVal = messageInput.value;
 
-    fetch(`http://localhost:8000/api/${offerId}/comments`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            [csrfHeaderName]: csrfHeaderValue
-        },
-        body: JSON.stringify({
-            message: messageVal
+    if (messageVal.trim().length > 0) {
+        fetch(`http://localhost:8000/api/${offerId}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                [csrfHeaderName]: csrfHeaderValue
+            },
+            body: JSON.stringify({
+                message: messageVal
+            })
         })
-    }).then(res => res.json())
-        .then(data => {
-            document.getElementById('message').value = ""
-            commentContaier.innerHTML += comementAsHtml(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                messageInput.value = "";
+                commentContaier.innerHTML += comementAsHtml(data);
+                updatePostCommentButtonColor();
+            });
+    }
 }
 
 function comementAsHtml(comment) {
-    let commentHtml = '<div>\n'
-    commentHtml += `<h4>${comment.authorName}</h4>\n`
-    commentHtml += `<p>${comment.message}</p>\n`
-    commentHtml += '</div>\n'
+    let commentHtml = '<div>\n';
+    commentHtml += `<h4>${comment.authorName}</h4>\n`;
+    commentHtml += `<p>${comment.message}</p>\n`;
+    commentHtml += '</div>\n';
 
-    return commentHtml
+    return commentHtml;
 }
 
 fetch(`http://localhost:8000/api/${offerId}/comments`, {
     headers: {
         "Accept": "application/json"
     }
-}).then(res => res.json())
+})
+    .then(res => res.json())
     .then(data => {
-        for(let comment of data) {
-            commentContaier.innerHTML += comementAsHtml(comment)
+        for (let comment of data) {
+            commentContaier.innerHTML += comementAsHtml(comment);
         }
-    })
+        updatePostCommentButtonColor();
+    });
+
+function updatePostCommentButtonColor() {
+    const messageVal = messageInput.value;
+    if (messageVal.trim().length === 0) {
+        postCommentButton.style.backgroundColor = 'grey';
+    } else {
+        postCommentButton.style.backgroundColor = 'blue';
+    }
+}
+
+// Invoke the function whenever the input value changes
+messageInput.addEventListener('input', updatePostCommentButtonColor);
